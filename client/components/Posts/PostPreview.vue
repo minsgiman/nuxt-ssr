a95d6a4f1b28111ca79e04ab2ba897b95f23e8bd
mskang <minsgiman@gmail.com>
2020-04-14 18:10:50 +0900

    관리자 페이지

--- client/components/Posts/PostPreview.vue
+++ client/components/Posts/PostPreview.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 article.post-preview
-	nuxt-link(:to="'/posts/' + id")
+	nuxt-link(:to="postLink")
 		figure.post-thumbnail(:style="{'background-image': 'url('+ thumbnail +')'}")
 		.post-content
 			h2.post-title {{ title }}
@@ -26,9 +26,16 @@ export default {
     content: {
       type: String,
       required: true
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true
     }
   },
   computed: {
+    postLink() {
+      return this.isAdmin ? '/admin/' + this.id : '/posts/' + this.id
+    },
     ellipseContent() {
       const ellipseText =
         this.content.length < 65
